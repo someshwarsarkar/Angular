8cb5dd0c68aeea7504722881eb8ca88b702be8c6
someshwarsarkar <60778665+someshwarsarkar@users.noreply.github.com>
2020-02-13 16:52:19 +0530

    darun

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,26 +13,5 @@ export class LoginComponent implements OnInit {
   constructor(private routes:Router, private js:SeralbumsService) { }
 
   ngOnInit() {
-    this.loginForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      password: new FormControl(null, [Validators.required, Validators.maxLength(8)])
-  });
   }
-  loginUser() {
-  
-  console.log(this.loginForm.value);
-}
-check(username: string, password : string)
-  {
-    var output = this.js.checklogin(username, password);
-    console.log(output);
-    if(output == true)
-    {
-      this.routes.navigate(['userdetails']);
-    }
-    else{
-this.msg ='Invalid username or password';
-    }
-  }
-  
 }
